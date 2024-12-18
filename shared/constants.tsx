a7850ee4213ce202de7a66683a6ac8d11943b6bb
nickolasgough <nickolas.v.gough@gmail.com>
2024-12-18 19:08:01 +0000

    Remove the auth from the request.

--- shared/constants.tsx
+++ shared/constants.tsx
@@ -9,7 +9,7 @@ export const gitHubRequestHeaders = {
   headers: {
     "Accept": "application/vnd.github+json",
     "X-GitHub-Api-Version": "2022-11-28",
-    "Authorization": `Bearer ${gitHubAccessToken}`,
+    // "Authorization": `Bearer ${gitHubAccessToken}`,
   }
 };
 
